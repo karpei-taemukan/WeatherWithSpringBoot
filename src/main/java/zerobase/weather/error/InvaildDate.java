c04313b26f782ead8685ff5adfe46c7d57942589
karpei-taemukan <l07wkdgustn07l@gmail.com>
2024-03-03 14:59:03 +0900

    wheather project commit

--- src/main/java/zerobase/weather/error/InvaildDate.java
+++
@@ -1,9 +0,0 @@
-package zerobase.weather.error;
-
-public class InvaildDate extends RuntimeException {
-    private static final String MESSAGE = "TOO FAR FUTURE";
-
-    public InvaildDate(){
-        super(MESSAGE);
-    }
-}
