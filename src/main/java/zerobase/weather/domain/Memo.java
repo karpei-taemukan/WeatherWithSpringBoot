8586910d168e0bf705b4c6981601eefc8c208e2d
karpei-taemukan <l07wkdgustn07l@gmail.com>
2024-03-03 15:00:09 +0900

    wheather project commit

---
+++ src/main/java/zerobase/weather/domain/Memo.java
@@ -0,0 +1,25 @@
+package zerobase.weather.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "memo")
+public class Memo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    /*
+    * GenerationType.IDENTITY -> spring boot 에서 만든키가 아닌 DB 에서 생성한 키를 따름
+    * */
+    private int id;
+    private String text;
+}
